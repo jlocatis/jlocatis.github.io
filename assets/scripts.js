a8c019178a976a285102536c55014aea25c8d738
Jamie Locatis <jlocatis@gmail.com>
2017-02-26 12:32:06 -0600

    More layout changes, fixed Twitter share link

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,38 +1,39 @@
-window.addEventListener("resize", function() {
-	windowWidth = document.documentElement.clientWidth;
-	stickyNav(windowWidth);
-});
-
 window.addEventListener("load", function() {
 	document.getElementById("hamburger").addEventListener("click", displayMenu);
-	windowWidth = document.documentElement.clientWidth;
-	stickyNav(windowWidth);
-	// if (windowWidth >= 650) {
-	// 	window.onscroll = function() {
- //   			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
- //   				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
- //   				document.getElementById("menu-items").id = "menu-items-sticky";
- //   			} else {
- //   				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
- //   				document.getElementById("menu-items-sticky").id = "menu-items";
- //   			}
-	// 	}
-	// }
-});
-
-function stickyNav(windowWidth) {
+	var windowWidth = document.documentElement.clientWidth;
 	if (windowWidth >= 650) {
 		window.onscroll = function() {
    			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
    				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
    				document.getElementById("menu-items").id = "menu-items-sticky";
+   				document.getElementById("nav-right").id = "nav-right-sticky";
    			} else {
    				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
    				document.getElementById("menu-items-sticky").id = "menu-items";
+   				document.getElementById("nav-right-sticky").id = "nav-right";
    			}
 		}
 	}
-}
+});
+
+// window.addEventListener("resize", function() {
+// 	windowWidth = document.documentElement.clientWidth;
+// 	stickyNav(windowWidth);
+// });
+
+// function stickyNav(windowWidth) {
+// 	if (windowWidth >= 650) {
+// 		window.onscroll = function() {
+//    			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
+//    				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
+//    				document.getElementById("menu-items").id = "menu-items-sticky";
+//    			} else {
+//    				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
+//    				document.getElementById("menu-items-sticky").id = "menu-items";
+//    			}
+// 		}
+// 	}
+// }
 
 function displayMenu() {
 	document.getElementById("hamburger").innerHTML = "<i class=\"fa fa-times-circle fa-2x\" aria-hidden=\"true\"></i>";
