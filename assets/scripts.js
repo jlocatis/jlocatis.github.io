280868fe903b146e08692be276614bbeff91f7fd
Jamie Locatis <jlocatis@gmail.com>
2017-02-25 19:12:33 -0600

    More updates

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,47 @@
-window.addEventListener("load", function() {
-	document.getElementById("hamburger").addEventListener("click", displayMenu);
+window.addEventListener("resize", function() {
+	windowWidth = document.documentElement.clientWidth;
+	stickyNav(windowWidth);
 });
 
+window.addEventListener("load", function() {
+	document.getElementById("hamburger").addEventListener("click", displayMenu);
+	windowWidth = document.documentElement.clientWidth;
+	stickyNav(windowWidth);
+	// if (windowWidth >= 650) {
+	// 	window.onscroll = function() {
+ //   			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
+ //   				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
+ //   				document.getElementById("menu-items").id = "menu-items-sticky";
+ //   			} else {
+ //   				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
+ //   				document.getElementById("menu-items-sticky").id = "menu-items";
+ //   			}
+	// 	}
+	// }
+});
+
+function stickyNav(windowWidth) {
+	if (windowWidth >= 650) {
+		window.onscroll = function() {
+   			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
+   				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
+   				document.getElementById("menu-items").id = "menu-items-sticky";
+   			} else {
+   				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
+   				document.getElementById("menu-items-sticky").id = "menu-items";
+   			}
+		}
+	}
+}
+
 function displayMenu() {
-	document.getElementsByClassName("menu-bar")[0].style.top = "100px";
-	document.getElementsByClassName("menu-bar")[0].style.opacity = "1";
 	document.getElementById("hamburger").innerHTML = "<i class=\"fa fa-times-circle fa-2x\" aria-hidden=\"true\"></i>";
 	document.getElementById("hamburger").removeEventListener("click", displayMenu);
 	document.getElementById("hamburger").addEventListener("click", collapseMenu);
-	document.getElementsByClassName("page-body")[0].addEventListener("click", collapseMenu);
 }
 
 function collapseMenu() {
-	document.getElementsByClassName("menu-bar")[0].style.top = "45px";
-	document.getElementsByClassName("menu-bar")[0].style.opacity = "0";
 	document.getElementById("hamburger").innerHTML = "<i class=\"fa fa-bars fa-2x\" aria-hidden=\"true\"></i>";
-	document.getElementsByClassName("page-body")[0].removeEventListener("click", collapseMenu);
-	document.getElementById("hamburger").removeEventListener("click", collapseMenu);
 	document.getElementById("hamburger").addEventListener("click", displayMenu);
+	document.getElementById("hamburger").removeEventListener("click", collapseMenu);
 }
