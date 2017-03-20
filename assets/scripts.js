edc6c7294154829f24d72c4db6cdadeafa1d1b49
Jamie Locatis <jlocatis@gmail.com>
2017-03-20 15:35:40 -0500

    Merging from another computer

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,37 +3,26 @@ window.addEventListener("load", function() {
 	var windowWidth = document.documentElement.clientWidth;
 	if (windowWidth >= 650) {
 		window.onscroll = function() {
-   			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
-   				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
-   				document.getElementById("menu-items").id = "menu-items-sticky";
-   				document.getElementById("nav-right").id = "nav-right-sticky";
-   			} else {
-   				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
-   				document.getElementById("menu-items-sticky").id = "menu-items";
-   				document.getElementById("nav-right-sticky").id = "nav-right";
-   			}
+   			// if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
+   			// 	document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
+   			// 	document.getElementById("menu-items").id = "menu-items-sticky";
+   			// 	document.getElementById("nav-right").id = "nav-right-sticky";
+   			// } else {
+   			// 	document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
+   			// 	document.getElementById("menu-items-sticky").id = "menu-items";
+   			// 	document.getElementById("nav-right-sticky").id = "nav-right";
+   			// }
+   			$(document).ready(function() {
+        		var $header = $("header"),
+            	$clone = $header.before($header.clone().addClass("clone"));
+        		$(window).on("scroll", function() {
+            		var fromTop = $(window).scrollTop();
+            		$("body").toggleClass("down", (fromTop > 400));
+        		});
+    		});
 		}
 	}
 });
-
-// window.addEventListener("resize", function() {
-// 	windowWidth = document.documentElement.clientWidth;
-// 	stickyNav(windowWidth);
-// });
-
-// function stickyNav(windowWidth) {
-// 	if (windowWidth >= 650) {
-// 		window.onscroll = function() {
-//    			if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
-//    				document.getElementsByClassName("menu-bar")[0].className = "menu-bar-sticky";
-//    				document.getElementById("menu-items").id = "menu-items-sticky";
-//    			} else {
-//    				document.getElementsByClassName("menu-bar-sticky")[0].className = "menu-bar";
-//    				document.getElementById("menu-items-sticky").id = "menu-items";
-//    			}
-// 		}
-// 	}
-// }
 
 function displayMenu() {
 	document.getElementById("hamburger").innerHTML = "<i class=\"fa fa-times-circle fa-2x\" aria-hidden=\"true\"></i>";
